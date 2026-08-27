828564f8aff8293bbe604da85f79c4f833db51bb
Joel Guillen <noctis2001@gmail.com>
2026-08-27 19:59:57 -0400

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Screen from "./components/Screen.vue";
+</script>
+
+<template>
+  <Screen>
+    <Header />
+  </Screen>
+</template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <header
+    class="flex w-full h-16 px-6 justify-between bg-amber-500 items-center"
+  >
+    <div class="flex bg-amber-300">
+      <h2 class="">uApinnion</h2>
+    </div>
+
+    <div class="flex flex-1 items-center justify-end gap-4">
+      <div class="flex justify-between gap-3">
+        <a href="/"> Libros </a>
+        <a href="/"> Peliculas </a>
+        <a href="/"> Animes </a>
+      </div>
+
+      <div class="flex justify-around gap-2">
+        <button>Lib</button>
+        <button>Metrics</button>
+      </div>
+
+      <div>
+        <button>claro</button>
+      </div>
+    </div>
+  </header>
+</template>
