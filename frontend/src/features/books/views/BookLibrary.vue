31005c9f1e24ceba50057141e0f4a2ad57bc1b38
Joel Guillen <noctis2001@gmail.com>
2026-09-06 00:54:23 -0400

    feat(frontend):
    ° add pagination with ellipsis.
    ° add error message when there is a error loading books.

--- frontend/src/features/books/views/BookLibrary.vue
+++ frontend/src/features/books/views/BookLibrary.vue
@@ -9,10 +9,14 @@ const searchQuery = ref("");
 const debounceSearch = useDebounce(searchQuery, 500);
 const page = ref(1);
 
-const { data: trendingBooks, isLoading: isLoadingTrending } = useQuery({
+const {
+  data: trendingBooks,
+  isLoading: isLoadingTrending,
+  isError: isErrorTrending,
+} = useQuery({
   queryKey: ["books", "trending"],
   queryFn: apiBooks.getTrendingBooks,
-  staleTime: 1000 * 60 * 5,
+  staleTime: 1000 * 60 * 10,
   enabled: computed(() => !debounceSearch.value.trim()),
 });
 
@@ -20,18 +24,26 @@ const {
   data: searchBooks,
   isLoading: isLoadingSearch,
   isFetching: isFetchingSearch,
+  isError: isErrorSearch,
 } = useQuery({
   queryKey: ["books", "search", debounceSearch, page],
   queryFn: () => apiBooks.searchBooks(debounceSearch.value, page.value, 12),
-  staleTime: 1000 * 60 * 5,
+  staleTime: 1000 * 60 * 10,
   enabled: computed(() => !!debounceSearch.value.trim()),
   placeholderData: keepPreviousData,
 });
 
-const isLoadingBooks = computed(
-  () => isLoadingTrending.value || isLoadingSearch.value,
-);
+const isLoadingBooks = computed(() => {
+  return debounceSearch.value.trim()
+    ? isLoadingSearch.value
+    : isLoadingTrending.value;
+});
 
+const isErrorBooks = computed(() => {
+  return debounceSearch.value.trim()
+    ? isErrorSearch.value
+    : isErrorTrending.value;
+});
 const booksToDisplay = computed(() => {
   return debounceSearch.value.trim()
     ? searchBooks.value?.data || []
@@ -51,6 +63,51 @@ const nextPage = () => {
 watch(debounceSearch, () => {
   page.value = 1;
 });
+
+const visiblePages = computed(() => {
+  const total = totalPages.value;
+  const currentPage = page.value;
+
+  if (total <= 1) return total === 1 ? [1] : [];
+
+  const pages = [];
+  const firstPage = 1;
+  const lastPage = total;
+  const previousPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+
+  pages.push(firstPage);
+
+  if (Math.abs(firstPage - previousPage) > 1) {
+    pages.push("...");
+  }
+
+  if (firstPage < previousPage) {
+    pages.push(previousPage);
+  }
+
+  if (currentPage !== firstPage && currentPage !== lastPage) {
+    pages.push(currentPage);
+  }
+
+  if (nextPage < lastPage) {
+    pages.push(nextPage);
+  }
+
+  if (Math.abs(nextPage - lastPage) > 1) {
+    pages.push("...");
+  }
+
+  pages.push(lastPage);
+
+  return pages;
+});
+
+const goToPage = (numberPage) => {
+  if (numberPage >= 1 && numberPage <= totalPages.value) {
+    page.value = numberPage;
+  }
+};
 </script>
 
 <template>
@@ -62,8 +119,16 @@ watch(debounceSearch, () => {
         class="h-10 w-1/2 mt-2 border border-gray-300 dark:border-gray-400 focus:outline-gray-300 dark:focus:outline-gray-400 focus:outline-2 focus:outline-offset-2 ml-2 p-2 rounded-md transition-all text-black dark:text-white"
       />
     </section>
+
     <div v-if="isLoadingBooks" class="text-black dark:text-white mt-4 ml-2">
       <p>Están cargando los libros...</p>
+    </div>
+
+    <div v-else-if="isErrorBooks" class="text-red-500 mt-4 ml-2">
+      <p v-if="debounceSearch.trim()">
+        Error con el servidor al buscar los libros.
+      </p>
+      <p v-else>Error al traer los libros del servidor.</p>
     </div>
     <section
       v-else
@@ -73,7 +138,7 @@ watch(debounceSearch, () => {
         v-for="book in booksToDisplay"
         :key="book.key"
         class="flex gap-1.5 h-40 bg-gray-200 dark:bg-gray-600 rounded-lg p-2 overflow-hidden"
-        :class="{ 'opaccity-50 pointer-events-none': isFetchingSearch }"
+        :class="{ 'opacity-50 pointer-events-none': isFetchingSearch }"
       >
         <div
           class="w-24 h-full shrink-0 bg-gray-300 rounded-md overflow-hidden relative"
@@ -116,9 +181,26 @@ watch(debounceSearch, () => {
       >
         Anterior
       </button>
-      <span class="text-black dark:text-white"
-        >{{ page }} - {{ totalPages }}</span
-      >
+      <template v-for="(p, i) in visiblePages">
+        <button
+          v-if="p !== '...'"
+          :key="'btn-' + i"
+          @click="goToPage(p)"
+          class="px-2 py-2 bg-gray-200 dark:bg-gray-600 text-black dark:text-white rounded-md transition-colors duration-300"
+          :class="p === page ? 'bg-sky-400 text-white dark:bg-sky-800' : ''"
+        >
+          {{ p }}
+        </button>
+
+        <span
+          v-else
+          :key="'dots-' + i"
+          class="px-2 py-2 bg-gray-200 dark:bg-gray-600 text-black dark:text-white rounded-md select-none transition-colors duration-300"
+        >
+          {{ p }}
+        </span>
+      </template>
+
       <button
         @click="nextPage"
         :disabled="page === totalPages"
