5ada788462730c2b47bda9b115a687d0a95caf74
Joel Guillen <noctis2001@gmail.com>
2026-09-15 17:23:50 -0400

    fix(backend): allow request from frontendorigin in CORS setup.

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,8 +8,23 @@ from backend.models.review import Review
 from backend.models.book import Book
 
 from backend.config.db import get_db, Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, 
+    allow_methods=['*'], 
+    allow_headers=['*']
+)
 
 app.include_router(review_router)
 
