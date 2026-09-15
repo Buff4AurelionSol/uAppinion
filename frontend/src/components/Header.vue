814dc3751859131e1a72c83fb5585e6d5b6969d0
Joel Guillen <noctis2001@gmail.com>
2026-09-15 17:12:50 -0400

    style(frontend):
    ° implementing hover effects to text and icons in header.
    
    ° add button and icon to got to catalog view.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,14 +5,15 @@ import { Tv } from "@lucide/vue";
 import ThemeSwitch from "./ThemeSwitch.vue";
 import { ChartLine } from "@lucide/vue";
 import { Library } from "@lucide/vue";
+import { Search } from "@lucide/vue";
 
 import { useRoute } from "vue-router";
-import { computed, watchEffect } from "vue";
+import { computed } from "vue";
 
 const route = useRoute();
 
 const currentMedia = computed(() => route.path.split("/")[1] || "books");
-const currentView = computed(() => route.path.split("/")[2] || "library");
+const currentView = computed(() => route.path.split("/")[2] || "catalog");
 
 const isMediaActive = (media) => currentMedia.value === media;
 const isViewActive = (view) => currentView.value === view;
@@ -34,25 +35,29 @@ const isViewActive = (view) => currentView.value === view;
       >
         <li class="flex items-center w-auto">
           <router-link
-            class="flex items-center justify-between gap-2.5 w-auto text-sm rounded-lg transition-colors text-black dark:text-white"
-            :class="{
-              'bg-gray-100 dark:bg-gray-700 p-1.5': isMediaActive('books'),
-            }"
+            class="flex items-center justify-between gap-2.5 w-auto text-sm rounded-lg transition-colors hover:text-black dark:hover:text-white"
+            :class="[
+              isMediaActive('books')
+                ? 'bg-gray-100 dark:bg-gray-700 p-1.5 dark:text-white'
+                : 'text-gray-400 dark:text-gray-400',
+            ]"
             :to="`/books/${currentView}`"
           >
             <BookOpen
               stroke-width="2"
-              class="text-gray-500 dark:text-gray-700'"
+              class="text-gray-500 dark:text-gray-400"
             />
             Libros
           </router-link>
         </li>
         <li class="flex items-center">
           <router-link
-            class="flex items-center justify-between gap-2.5 w-auto text-sm rounded-lg transition-colors text-black dark:text-white"
-            :class="{
-              'bg-gray-100 dark:bg-gray-700 p-1.5': isMediaActive('movies'),
-            }"
+            class="flex items-center justify-between gap-2.5 w-auto text-sm rounded-lg transition-colors hover:text-black dark:hover:text-white"
+            :class="[
+              isMediaActive('movies')
+                ? 'bg-gray-100 dark:bg-gray-700 p-1.5 dark:text-white'
+                : 'text-gray-400 dark:text-gray-400',
+            ]"
             :to="`/movies/${currentView}`"
           >
             <Clapperboard
@@ -64,10 +69,12 @@ const isViewActive = (view) => currentView.value === view;
 
         <li class="flex items-center">
           <router-link
-            class="flex items-center justify-between gap-2.5 w-auto text-sm rounded-lg transition-colors text-black dark:text-white"
-            :class="{
-              'bg-gray-100 dark:bg-gray-700 p-1.5': isMediaActive('animes'),
-            }"
+            class="flex items-center justify-between gap-2.5 w-auto text-sm rounded-lg transition-colors hover:text-black dark:hover:text-white"
+            :class="[
+              isMediaActive('animes')
+                ? 'bg-gray-100 dark:bg-gray-700 p-1.5 dark:text-white'
+                : 'text-gray-400 dark:text-gray-400',
+            ]"
             :to="`/animes/${currentView}`"
           >
             <Tv
@@ -83,26 +90,56 @@ const isViewActive = (view) => currentView.value === view;
       >
         <li>
           <router-link
+            :to="`/${currentMedia}/catalog`"
+            class="block p-1 rounded-md transition-colors group"
+            :class="{
+              'bg-white shadow-sm dark:bg-gray-700': isViewActive('catalog'),
+            }"
+            ><Search
+              stroke-width="2"
+              :class="[
+                'text-gray-500',
+                isViewActive('catalog')
+                  ? 'dark:text-gray-400'
+                  : 'dark:text-gray-500',
+                'group-hover:text-gray-400',
+              ]"
+          /></router-link>
+        </li>
+        <li>
+          <router-link
             :to="`/${currentMedia}/library`"
-            class="block p-1 rounded-md transition-colors"
+            class="block p-1 rounded-md transition-colors group"
             :class="{
               'bg-white shadow-sm dark:bg-gray-700': isViewActive('library'),
             }"
             ><Library
-              class="text-gray-500 dark:text-gray-400'"
               stroke-width="2"
+              :class="[
+                'text-gray-500',
+                isViewActive('library')
+                  ? 'dark:text-gray-400'
+                  : 'dark:text-gray-500',
+                'group-hover:text-gray-400',
+              ]"
           /></router-link>
         </li>
         <li>
           <router-link
             :to="`/${currentMedia}/metrics`"
-            class="block p-1 rounded-md transition-colors"
+            class="block p-1 rounded-md transition-colors group"
             :class="{
               'bg-white shadow-sm dark:bg-gray-700': isViewActive('metrics'),
             }"
             ><ChartLine
-              class="text-gray-500 dark:text-gray-400"
               stroke-width="2"
+              :class="[
+                'text-gray-500',
+                isViewActive('metrics')
+                  ? 'dark:text-gray-400'
+                  : 'dark:text-gray-500',
+                'group-hover:text-gray-400',
+              ]"
             />
           </router-link>
         </li>
