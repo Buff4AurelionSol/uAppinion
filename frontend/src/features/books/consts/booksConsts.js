e3073d2f07a3bb2801403c26204e0424f3995211
Joel Guillen <noctis2001@gmail.com>
2026-09-25 16:27:43 -0400

    refactor(frontend): add styles property to statusesValues constant

--- frontend/src/features/books/consts/booksConsts.js
+++ frontend/src/features/books/consts/booksConsts.js
@@ -1,8 +1,28 @@
 export const statusesValues = [
-  { value: "read", label: "Leído" },
-  { value: "reading", label: "Leyendo" },
-  { value: "plan_to_read", label: "Quiero leer" },
-  { value: "dropped", label: "Abandonado" },
+  {
+    value: "read",
+    label: "Leído",
+    classes:
+      "text-sm bg-emerald-100 text-emerald-800 dark:bg-emerald-500/20 dark:text-emerald-300",
+  },
+  {
+    value: "reading",
+    label: "Leyendo",
+    classes:
+      "text-sm bg-amber-100 text-amber-800 dark:bg-amber-500/20 dark:text-amber-300",
+  },
+  {
+    value: "plan_to_read",
+    label: "Quiero leer",
+    classes:
+      "text-sm bg-gray-300 text-gray-700 dark:bg-gray-700 dark:text-gray-300",
+  },
+  {
+    value: "dropped",
+    label: "Abandonado",
+    classes:
+      "text-sm bg-rose-100 text-rose-800 dark:bg-rose-500/20 dark:text-rose-300",
+  },
 ];
 
 export const formatsValues = [
@@ -22,8 +42,6 @@ export const INITIAL_REVIEW_FORM = {
   text_review: "",
   is_read: false,
 };
-
-const GENRES_VALUES = {};
 
 export const ORDER_BY_VALUES = [
   { value: "created_at", label: "Más recientes" },
